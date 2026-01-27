1c4efc9babbf0d5fc3e46bb377f701a69b4e53b2
yeshvanth087 <yeshvanth087@gmail.com>
2026-01-27 14:59:42 +0530

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,16 +1,8 @@
 import random
 import string
-
-# Take password length from user
 length = int(input("Enter desired password length: "))
-
-# Define character set
 characters = string.ascii_letters + string.digits + string.punctuation
-
-# Generate password
 password = ""
 for i in range(length):
     password += random.choice(characters)
-
-# Display password
 print("Generated Password:", password)
